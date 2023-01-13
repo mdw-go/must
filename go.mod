beb6b00734a67e1babe3bd32e60ba71da814b3a7
Michael Whatcott <mike@smarty.com>
2023-01-13 13:39:27 -0700

    Initial commit.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mdwhatcott/must
+
+go 1.19
---
+++ must_test.go
@@ -0,0 +1,23 @@
+package must_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/mdwhatcott/must"
+)
+
+func TestNada(t *testing.T)   { defer catch(t); must.Nada(nada()) }
+func TestValue(t *testing.T)  { defer catch(t); _ = must.Value(value()) }
+func TestValues(t *testing.T) { defer catch(t); _, _ = must.Values(values()) }
+func catch(t *testing.T) {
+	if r := recover(); r.(error).Error() != err.Error() {
+		t.Error("should have panicked on err")
+	}
+}
+
+var err = errors.New("err")
+
+func nada() error               { return err }
+func value() (int, error)       { return 0, err }
+func values() (int, int, error) { return 0, 0, err }
